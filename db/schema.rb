0335581ad1de64708757340a6023c109d77adc62
Maciej Dobosz <dobosz.ma@interia.pl>
2019-12-29 14:08:30 +0100

    Create Images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_29_094657) do
+ActiveRecord::Schema.define(version: 2019_12_29_130351) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
